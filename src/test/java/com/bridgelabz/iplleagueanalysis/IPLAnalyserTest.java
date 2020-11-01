824adc98e4e5c9263069e601df96b1f8c57ab9aa
akashgupta3852 <akashgupta3852@gmail.com>
2020-11-02 01:39:16 +0530

    [Akash]Added: The test cases for wrong csv file path

--- src/test/java/com/bridgelabz/iplleagueanalysis/IPLAnalyserTest.java
+++ src/test/java/com/bridgelabz/iplleagueanalysis/IPLAnalyserTest.java
@@ -1,33 +1,56 @@
 package com.bridgelabz.iplleagueanalysis;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class IPLAnalyserTest {
-    private static final String IPL_MOST_WICKETS_CSV_FILE_PATH  = "C:\\Users\\Akash Gupta\\Downloads\\WP DP Data_02 IPL2019FactsheetMostWkts.csv";
-    private static final String IPL_MOST_RUNS_CSV_FILE_PATH  = "C:\\Users\\Akash Gupta\\Downloads\\WP DP Data_01 IPL2019FactsheetMostRuns.csv";
+	private static final String IPL_MOST_WICKETS_CSV_FILE_PATH = "C:\\Users\\Akash Gupta\\Downloads\\WP DP Data_02 IPL2019FactsheetMostWkts.csv";
+	private static final String IPL_MOST_RUNS_CSV_FILE_PATH = "C:\\Users\\Akash Gupta\\Downloads\\WP DP Data_01 IPL2019FactsheetMostRuns.csv";
+	private static final String WRONG_CSV_FILE_PATH_FOR_BOWLING = "C:\\Users\\Akash Gupta\\Download\\WP DP Data_02 IPL2019FactsheetMostWkts.csv";
+	private static final String WRONG_CSV_FILE_PATH_FOR_BATTING = "C:\\Users\\Akash Gupta\\Download\\WP DP Data_01 IPL2019FactsheetMostRuns.csv";
+	private IPLAnalyser iplAnalyser;
 
-    @Test
+	@Before
+	public void initialize() {
+		iplAnalyser = new IPLAnalyser();
+	}
+
+	@Test
 	public void givenBowlingCSVFile_ShouldReturnExactCount() {
 		try {
-			IPLAnalyser iplAnalyser = new IPLAnalyser();
-			int playerCount  = iplAnalyser.loadBowlingData(IPL_MOST_WICKETS_CSV_FILE_PATH);
+			int playerCount = iplAnalyser.loadBowlingData(IPL_MOST_WICKETS_CSV_FILE_PATH);
 			Assert.assertEquals(99, playerCount);
-			System.out.println("EQEsdghgggggggggggRF");
 		} catch (IPLAnalyserException e) {
 			e.printStackTrace();
 		}
 	}
-    
-    @Test
+
+	@Test
+	public void givenBowlingCSVFile_WithWrongFile_ShouldThrowException() {
+		try {
+			iplAnalyser.loadBowlingData(WRONG_CSV_FILE_PATH_FOR_BOWLING);
+		} catch (IPLAnalyserException e) {
+			Assert.assertEquals(IPLAnalyserException.ExceptionType.BOWLING_FILE_PROBLEM, e.type);
+		}
+	}
+
+	@Test
 	public void givenBattingCSVFile_ShouldReturnExactCount() {
 		try {
-			IPLAnalyser iplAnalyser = new IPLAnalyser();
-			int playerCount  = iplAnalyser.loadBattingData(IPL_MOST_RUNS_CSV_FILE_PATH);
+			int playerCount = iplAnalyser.loadBattingData(IPL_MOST_RUNS_CSV_FILE_PATH);
 			Assert.assertEquals(101, playerCount);
-			System.out.println("EQERF");
 		} catch (IPLAnalyserException e) {
 			e.printStackTrace();
+		}
+	}
+
+	@Test
+	public void givenBattingCSVFile_WithWrongFile_ShouldThrowException() {
+		try {
+			iplAnalyser.loadBattingData(WRONG_CSV_FILE_PATH_FOR_BATTING);
+		} catch (IPLAnalyserException e) {
+			Assert.assertEquals(IPLAnalyserException.ExceptionType.BATTING_FILE_PROBLEM, e.type);
 		}
 	}
 }
